b23e043a747055d5d805d636041f90e6678f652f
paras-017 <paraschauhan0017@gmail.com>
2023-05-25 12:02:44 +0530

    first commit

--- Strapi/films-frontend/src/app/components/Nav.js
+++ Strapi/films-frontend/src/app/components/Nav.js
@@ -1,7 +1,35 @@
+'use client'
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Nav = () => {
+  const [data, setData] = useState({identifier: '',password: ''});
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const responseData = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/auth/local`,
+     {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          identifier: data.identifier,
+          password: data.password,
+        }),
+      }
+    );
+    // setToken(responseData);
+  };
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+  const { user, loading } = useUser();
+  const logout = () => {
+    unsetToken();
+  };
+
 
   return (
     <nav className="flex flex-wrap items-center justify-between w-full py-4 md:py-0 px-4 text-lg text-gray-700 bg-white ">
@@ -20,7 +48,41 @@ const Nav = () => {
           <li>
             <Link href="/films" className="md:p-2 py-2 block hover:text-purple-400" >Films</Link>
           </li>
-          
+          {!loading && (user ? (
+              <li>
+                <Link href="/profile">
+                  <a className="md:p-2 py-2 block hover:text-purple-400">
+                    Profile
+                  </a>
+                </Link>
+              </li>
+            ) : (
+              ''
+            ))}
+          {!loading &&
+            (user ? (
+              <li>
+                <a className="md:p-2 py-2 block hover:text-purple-400" onClick={logout} style={{ cursor: 'pointer' }}>Logout</a>
+              </li>
+            ) : (''))}
+          {!loading && !user ? (
+            <>
+              <li>
+                <form onSubmit={handleSubmit} className="form-inline">
+                  <input type="text" name="identifier" onChange={handleChange} placeholder="Username" className="md:p-2 form-input py-2 rounded mx-2" required />
+                  <input type="password" name="password" onChange={handleChange} placeholder="Password" className="md:p-2 form-input py-2 rounded mx-2" required/>
+                  <button className="md:p-2 rounded py-2 text-black bg-purple-200 p-2" type="submit">
+                     Login
+                  </button>
+                </form>
+              </li>
+              <li>
+                <Link href="/register">
+                  <a className="md:p-2 block py-2 hover:text-purple-400 text-black">Register</a>
+                </Link>
+              </li>
+            </>
+          ) : ('')}
         </ul>
       </div>
     </nav>
